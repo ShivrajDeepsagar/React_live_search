90d48e46db16089515372ef05a85859264e42f0a
Shivraj <itscool.sid@gmail.com>
2018-02-05 13:11:02 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import CardList from './CardList';
+import {robots} from './robots';
+import Searchbox from './Searchbox';
+import './App.css';
+class App extends Component {
+  constructor(){
+    super()
+    this.state ={
+      robots: robots,
+      searchfield: ''
+    }
+  }
+
+  onSearchChange=(event)=>{
+    this.setState({searchfield: event.target.value})
+  }
+  render() {
+    const filterRobot= this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    return (
+      <div>
+      <h1 className='tc'>RoboFriends</h1>
+      <Searchbox searchChange={this.onSearchChange}/>
+      <CardList robots= {filterRobot}/>
+      </div>
+    );
+  }
+
+}
+
+export default App;
